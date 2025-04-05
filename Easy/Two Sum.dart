a6843b9f97731db6607e39d296d3878fd278a819
Muhammad Azam <161217248+azam-izm@users.noreply.github.com>
2025-04-05 10:27:43 +0500

    Update Two Sum.dart

--- Easy/Two Sum.dart
+++ Easy/Two Sum.dart
@@ -33,4 +33,30 @@ Only one valid answer exists.
 Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 */
 ```
+ List<int> twoSum(List<int> nums, int target) {
+  // Create a map to store the numbers we have seen so far
+  Map<int, int> seenNumbers = {};
+
+  // Iterate through the list
+  for (int i = 0; i < nums.length; i++) {
+    int complement = target - nums[i]; // The number we need to find
+
+    // Check if the complement exists in the map
+    if (seenNumbers.containsKey(complement)) {
+      return [seenNumbers[complement]!, i]; // Return the indices of the pair
+    }
+
+    // Add the current number and its index to the map
+    seenNumbers[nums[i]] = i;
+  }
+
+  return []; // If no solution is found (though the problem guarantees a solution)
+}
+
+void main() {
+  print(twoSum([2, 7, 11, 15], 9)); // Output: [0, 1]
+  print(twoSum([3, 2, 4], 6)); // Output: [1, 2]
+  print(twoSum([3, 3], 6)); // Output: [0, 1]
+}
+
 ```
